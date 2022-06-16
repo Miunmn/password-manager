863277858cc3cf278b65dc9cbd37e8a44cd1c874
miunmnn <esteban.principe@utec.edu.pe>
2022-06-16 17:19:44 -0500

    Login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,98 +1,17 @@
-import logo from './logo.svg';
-
-import React, { useState } from "react";
-
 import './App.css';
+import { Routes, Route} from "react-router-dom";
 
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Grid from "@mui/material/Grid"
-import { Button } from "@mui/material";
-import Modal from '@mui/material/Modal';
-
+//Pages
+import Login from './pages/Login/Login';
 
 function App() {
-  const [userString, setUserString] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmAccountModal, setConfirmAccountModal] = useState(false)
-  const [confCode, setConfcode] = useState("")
-
-
-  const ModalStyles = () => {
-    return { 
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: "90%",
-      maxWidth: "200px",
-      bgcolor: 'background.paper',
-      border: '2px solid #000',
-      boxShadow: 24,
-      p: 4
-    }
-  }
-
 
   return (
-    <>
-            
-    <div className="theme-light" data-gradient="gradient-1">
-        <div id="page">
-            <div id='content'>
-            <div className="page-content header-clear-medium d-flex justify-content-center">
-                <Box
-                    component="form"
-                    style={{ width: "70%" }}
-                    noValidate
-                    autoComplete="off"
-                >
-                    <h2>
-                        Login
-                    </h2>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            {/* <Item> */}
-                            <TextField fullWidth value={userString || ''} onChange={(e) => setUserString(e.target.value)} id="outlined-basic" label="User" variant="filled" />
-                            {/* </Item> */}
-                        </Grid>
-                        <Grid item xs={12}>
-                            {/* <Item> */}
-                            <TextField type='password' value={password || ''} onChange={(e) => setPassword(e.target.value)} fullWidth id="filled-basic" label="Password" variant="filled" />
-                            {/* </Item> */}
-                        </Grid>
-                        {/* <Grid style={{float:"rigth"}}> */}
-                        {/* </Grid> */}
-                    </Grid>
-                    <Button className="bg-green-primary" style={{ marginTop: "10%", width: "50%", float: "inherit" }}  variant="contained">Log in</Button>
-      
-                </Box>
-            </div>
-            </div>
-     
-        </div>
-        
-        <Modal
-            open={confirmAccountModal}
-            // onClose={handleCloseM}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-        <Box sx={ModalStyles}>
-            <div style={{ marginBottom: "15%" }}>
-                <Typography >Verification Code</Typography>
-            </div>
-            <TextField
-                value={confCode}
-                onChange={(e) => setConfcode(e.target.value)}
-                fullWidth
-                id="outlined-basic" label="Code" variant="outlined" />
-            <Button className="bg-green-primary" style={{ marginTop: "10%", width: "50%", float: "right" }}  variant="contained">Confirm Account</Button>   
-        </Box>
-        </Modal>
+    <div className='App'>
+        <Routes>
+            <Route exact path='/' element={<Login />}/>           
+        </Routes>
     </div>
-</>
   );
 }
 
---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,92 @@
+
+import React, { useState } from "react";
+
+import './Login.css';
+
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Grid from "@mui/material/Grid"
+import { Button } from "@mui/material";
+import Modal from '@mui/material/Modal';
+
+const Login = () => {
+  const [userString, setUserString] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmAccountModal, setConfirmAccountModal] = useState(false)
+  const [confCode, setConfcode] = useState("")
+
+
+  const ModalStyles = () => {
+    return { 
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: "90%",
+      maxWidth: "200px",
+      bgcolor: 'background.paper',
+      border: '2px solid #000',
+      boxShadow: 24,
+      p: 4
+    }
+  }
+
+  return (
+    <>
+            
+    <div className="theme-light" data-gradient="gradient-1">
+        <div id="page">
+            <div id='content'>
+            <div className="page-content header-clear-medium d-flex justify-content-center">
+                <Box
+                    component="form"
+                    style={{ width: "70%" }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <h2>
+                        Login
+                    </h2>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField fullWidth value={userString || ''} onChange={(e) => setUserString(e.target.value)} id="outlined-basic" label="User" variant="filled" />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField type='password' value={password || ''} onChange={(e) => setPassword(e.target.value)} fullWidth id="filled-basic" label="Password" variant="filled" />
+                        </Grid>
+                    </Grid>
+                    <Button className="bg-green-primary" style={{ marginTop: "10%", width: "50%", float: "inherit" }}  variant="contained">Log in</Button>
+      
+                </Box>
+            </div>
+            </div>
+     
+        </div>
+        
+        <Modal
+            open={confirmAccountModal}
+            // onClose={handleCloseM}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+        <Box sx={ModalStyles}>
+            <div style={{ marginBottom: "15%" }}>
+                <Typography >Verification Code</Typography>
+            </div>
+            <TextField
+                value={confCode}
+                onChange={(e) => setConfcode(e.target.value)}
+                fullWidth
+                id="outlined-basic" label="Code" variant="outlined" />
+            <Button className="bg-green-primary" style={{ marginTop: "10%", width: "50%", float: "right" }}  variant="contained">Confirm Account</Button>   
+        </Box>
+        </Modal>
+    </div>
+</>
+
+  )
+}
+
+
+export default Login;
