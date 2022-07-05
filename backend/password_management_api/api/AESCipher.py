b44a0492f8c1e8b5f2f18e44b486fda5110d90c3
josedlz <jose.delama@utec.edu.pe>
2022-07-05 00:10:59 -0500

    encryption decryption

--- backend/password_management_api/api/AESCipher.py
+++ backend/password_management_api/api/AESCipher.py
@@ -1,27 +1,23 @@
 from Crypto.Cipher import AES
+from Crypto import Random
 import hashlib
 import base64
 from django.db.models import Q
-from api.resources import User
+from api.resources import User, PasswordsManagerView
 class AESCipher:
-
     def __init__(self, key): 
         self.bs = AES.block_size
         self.key = hashlib.sha256(key.encode()).digest()
 
-    def encrypt(self, raw, request):
-      username = request.data['username']
-
+    def encrypt(self, raw):
       raw = self._pad(raw)
-      iv = User.objects.filter(Q(username=username))[0].iv
+      iv = Random.new().read(AES.block_size)
       cipher = AES.new(self.key, AES.MODE_CBC, iv)
       return base64.b64encode(iv + cipher.encrypt(raw.encode()))
 
-    def decrypt(self, enc, request):
-      username = request.data['username']
-
+    def decrypt(self, enc):
       enc = base64.b64decode(enc)
-      iv = User.objects.filter(Q(username=username))[0].iv
+      iv = enc[:AES.block_size]
       cipher = AES.new(self.key, AES.MODE_CBC, iv)
       return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
 
